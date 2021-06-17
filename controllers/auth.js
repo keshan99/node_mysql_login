3e45b2de661ada6dd665dff294fe7d3d946da271
keshan_mwm <mwmadushakeshan99@gmail.com>
2021-06-17 09:48:11 +0530

    init

---
+++ controllers/auth.js
@@ -0,0 +1,122 @@
+const mysql = require("mysql");
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+    port: '3307',
+
+});
+
+db.connect((error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("My sql connected.sss..");
+    }
+})
+
+
+exports.login = async(req, res) => {
+    try {
+
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).render('login', {
+                message: 'Please provide an email and passsword'
+            })
+        }
+
+        db.query('SELECT * FROM users WHERE email = ?', [email], async(error, results) => {
+            console.log(results);
+            if (results.length == 0) {
+                res.status(401).render('login', {
+                    message: 'Email is incorrect'
+                })
+            } else if (!(await bcrypt.compare(password, results[0].password))) {
+                res.status(401).render('login', {
+                    message: 'password is incorrect'
+                })
+            } else {
+                const id = results[0].id;
+
+                const token = jwt.sign({ id: id }, process.env.JWT_SECRET, {
+                    expiresIn: process.env.JWT_EXPIRES_IN
+                });
+
+                console.log('the token is: ' + token);
+
+                const cookieOptions = {
+                    expires: new Date(
+                        Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+                    ),
+                    httpOnly: true
+                }
+
+                res.cookie('jwt', token, cookieOptions);
+                res.status(200).redirect("/");
+
+            }
+
+
+
+
+        })
+
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+exports.register = (req, res) => {
+    console.log(req.body);
+
+    //const name = res.body.name;
+    //const email = res.body.email;
+    //const password = res.body.password;
+    //const passwordConform = res.body.passwordConform;
+    const { name, email, password, passwordConform } = req.body;
+
+    db.query('SELECT email FROM users WHERE email = ?', [email], async(error, result) => {
+        if (error) {
+            console.log(error);
+        }
+
+        if (result.length > 0) {
+            return res.render('register', {
+                message: 'That email is already in use'
+            })
+        } else if (password !== passwordConform) {
+            return res.render('register', {
+                message: 'Password do not match'
+            });
+        }
+
+        let hashedPassword = await bcrypt.hash(password, 8);
+        console.log(hashedPassword);
+
+
+
+        db.query('INSERT INTO users SET ?', { name: name, email: email, password: hashedPassword }, (errror, result) => {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(result);
+                return res.render('register', {
+                    message: 'User registered'
+                });
+            }
+        })
+
+    });
+
+    //res.send("Form submited...");
+
+}
