e218f5e688103f9d107ee84a3513e274c0530693
keshan_mwm <mwmadushakeshan99@gmail.com>
2021-06-18 10:55:18 +0530

    logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -156,3 +156,12 @@ exports.isLoggedIn = async(req, res, next) => {
     }
 
 }
+
+exports.logout = async(req, res) => {
+    res.cookie('jwt', 'logout', {
+        expires: new Date(Date.now() + 2 * 1000),
+        httpOnly: true
+    })
+
+    res.status(200).redirect('/')
+}
